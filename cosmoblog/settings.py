c7e696f458c8cd13aba496adb92ff7eb38e51893
David Ezeani <davidezeani11@gmal.com>
2024-12-22 12:30:45 +0100

    allowed host update

--- cosmoblog/settings.py
+++ cosmoblog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["cosmoblog.onrender.com"]
+ALLOWED_HOSTS = ["cosmoblog-obr9.onrender.com"]
 
 
 # Application definition
